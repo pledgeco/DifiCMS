bbda10bd7da2731b07e8f10773043f050721f522
pledgeco <vegard1990@gmail.com>
2015-03-26 10:08:04 +0100

    Index og Global
    Kodet strutkuren til global og lagd ferdig design for index

---
+++ app/tpl/skins/Difi/index.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head> 
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+<link rel="shortcut icon" href="http://www.difi.no/profiles/difino/themes/difi3_difino/favicon.ico" type="image/vnd.microsoft.icon" />
+<link rel="stylesheet" href="{url}/css/style.css" type="text/css">
+<title>{difiname}</title> 
+</head>
+
+<body>
+<style type="text/css">
+body {
+	background: #ffffff;
+	font-size:12px;
+}
+
+.wrapper {
+	width: 1000px;
+	margin-left: auto;
+    margin-right: auto;
+}
+
+#inside-wrap {
+	width:100%;
+
+}
+
+#right-wrapper {
+	float:right; 
+	width:430px; 
+
+
+}
+
+#left-wrapper {
+	float:left; 
+	width: 500px;
+}
+
+#logo {
+	background-image: url("images/logo2.png");
+	height:60px;
+	width:350px;
+	float:left;
+	margin-top:15px;
+
+}
+
+#search-button {
+	background-image: url("images/search-2.png");
+	height:34px;
+	width:70px;
+	float:right;
+	margin-right:120px;
+}
+
+#search-button:hover {
+	background-image: url("images/search-hover3.png");
+	height:34px;
+	width:70px;
+	float:right;
+	margin-right:120px;
+}
+
+#projectregister {
+	background-image: url("images/register-project.png");
+	height:44px;
+	width:173px;
+	float:left;
+	
+}
+
+#projectregister:hover {
+	background-image: url("images/register-project-hover.png");
+	height:44px;
+	width:173px;
+	float:left;
+	
+}
+
+#menu {
+	float:right;
+
+}
+
+ul#nav li { /*Setter at liste elementet ikke skal ha noen stil på seg. Setter også at det skal være 15 pixler mellom hver lenke i meny*/
+	list-style-type: none;
+	display: inline-block;
+	margin-left: 15px;
+	margin-top:51px;
+}
+
+a:link {
+    text-decoration: none;
+}
+
+a:visited {
+    text-decoration: none;
+}
+
+a {	
+	font-size:12px;
+	color:#393a3a;
+	font-weight: bold;
+}
+
+a:hover {
+	color:#868889;
+	font-weight: bold;
+}
+
+.content {
+    overflow: hidden;
+    margin-top:20px;
+    margin-bottom:9px;
+}
+
+.content img {
+    float: left;
+}
+
+#projext-box
+{
+     position: relative; 
+   		width: 100%;
+}
+
+
+#text
+{
+    z-index:100;
+    position:absolute;    
+    color:white;
+    font-size:24px;
+    font-weight:bold;
+    left:150px;
+    top:350px;
+}
+
+
+	.wrap {
+	  /* force the div to properly contain the floated images: */
+	  position:relative;
+	  float:left;
+	  clear:none;
+	  overflow:hidden;
+	}
+	.wrap img {
+	  position:relative;
+	  z-index:1;
+	  margin-bottom:5px;
+	  margin-left: 3px;
+
+	}
+	.wrap .desc {
+	  display:block;
+	  position:absolute;
+	  width:100%;
+	  top:24%;
+	  left:0;
+	  z-index:2;
+	  text-align:center;
+	  color:white;
+
+	}
+
+
+</style>
+
+
+<div class="wrapper">
+	<a id="logo" href="http://prosjektdatabasen.com"></a>
+
+<div id="menu">
+	<ul id="nav"> <!--  meny  -->
+                <li><a href="#"> Hjem</a></li>
+                <li><a href="#"> Registrer et prosjekt</a></li>
+                <li><a href="#"> Kontakt oss</a></li>
+		<li><a href="#" data-reveal-id="#"> Logg inn</a></li>
+	</ul>          
+</div>
+
+		<br /><br /><br /><br /><br /><br /><br />
+	<hr />
+<br />
+
+
+	<div id="inside-wrap">
+    	<div id="left-wrapper">
+    		<h2 style="color:#2e7297; font-size:20px;">Prosjektdatabasen</h2>
+    		<p style="color:#424242;">Søk etter prosjekter i prosjektveiviseren</p>
+    		<div id="search-button"></div>
+    		<input type="text" name="lastname" placeholder="Skriv inn søket ditt..." 
+    		style="color:#bebdbd; width:280px; padding:10px; border: 1px solid;border-radius: 4px;"> 
+			<br /><br />
+
+			<div class="content">
+    		<img src="images/circle-new.png"/ alt="" >
+    		<h2 style="color:#2e7297; margin-top:9px; font-size:16px;">Siste 12 prosjekter</h2>
+    		
+			</div>​
+
+
+
+
+<div class="wrap">
+	<img src="images/projects.png" />
+	<h3 class="desc">Image 1</h3>
+
+</div>
+<div class="wrap">
+	<img src="images/projects.png" />
+	<h3 class="desc">Image 2</h3>
+
+</div>
+<div class="wrap">
+	<img src="images/projects.png" />
+	<h3 class="desc">Image 3</h3>
+
+</div>
+<div class="wrap">
+	<img src="images/projects.png" />
+	<h3 class="desc">Image 1</h3>
+
+</div>
+<div class="wrap">
+	<img src="images/projects.png" />
+	<h3 class="desc">Image 2</h3>
+
+</div>
+<div class="wrap">
+	<img src="images/projects.png" />
+	<h3 class="desc">Image 3</h3>
+
+</div>
+<div class="wrap">
+	<img src="images/projects.png" />
+	<h3 class="desc">Image 1</h3>
+
+</div>
+<div class="wrap">
+	<img src="images/projects.png" />
+	<h3 class="desc">Image 2</h3>
+
+</div>
+<div class="wrap">
+	<img src="images/projects.png" />
+	<h3 class="desc">Image 3</h3>
+
+</div>
+<div class="wrap">
+	<img src="images/projects.png" />
+	<h3 class="desc">Image 1</h3>
+
+</div>
+<div class="wrap">
+	<img src="images/projects.png" />
+	<h3 class="desc">Image 2</h3>
+
+</div>
+<div class="wrap">
+	<img src="images/projects.png" />
+	<h3 class="desc">Image 3</h3>
+
+</div>
+<div class="content" style="margin-top:255px;">
+    		<img src="images/circle-new.png"/ alt="" >
+    		<h2 style="color:#2e7297; margin-top:9px; font-size:16px;">Registrer et prosjekt</h2>
+    		Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard
+    		 for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage et, trykk på knappen under
+for å registrere et prosjekt.
+<br /><br /><br />
+		<a id="projectregister" href="http://prosjektdatabasen.com"></a>
+
+			</div>​
+
+    	 	</div> 
+
+   		 <div id="right-wrapper">
+   		 	<div class="content">
+    		<img src="images/circle-new.png"/ alt="" >
+    		<h2 style="color:#2e7297; margin-top:9px; font-size:16px;">Hva er prosjektdatabasen?</h2>
+    		Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens 
+    		standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage 
+    		et prøveeksemplar av en bok. Lorem Ipsum har tålt tidens tann usedvanlig godt, og har i tillegg til å bestå gjennom
+    		 fem århundrer også tålt spranget over til elektronisk typografi uten vesentlige endringer. Lorem Ipsum ble gjort allment 
+    		 kjent i 1960-årene ved lanseringen av Letraset-ark med avsnitt fra Lorem Ipsum, og senere med sideombrekkingsprogrammet Aldus 
+    		 PageMaker som tok i bruk nettopp Lorem Ipsum for dummytekst.
+			</div>​
+
+			<div class="content">
+    		<img src="images/circle-new.png"/ alt="" >
+    		<h2 style="color:#2e7297; margin-top:9px; font-size:16px;">Nyheter</h2>
+    		Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens 
+    		standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage 
+    		et prøveeksemplar av en bok. Lorem Ipsum har tålt tidens tann usedvanlig godt, og har i tillegg til å bestå gjennom
+    		 fem århundrer også tålt spranget over til elektronisk typografi uten vesentlige endringer. Lorem Ipsum ble gjort allment 
+    		 kjent i 1960-årene ved lanseringen av Letraset-ark med avsnitt fra Lorem Ipsum, og senere med sideombrekkingsprogrammet Aldus 
+    		 PageMaker som tok i bruk nettopp Lorem Ipsum for dummytekst.
+
+    		 
+			</div>​
+
+			<div class="content">
+    		<img src="images/circle-new.png"/ alt="" >
+    		<h2 style="color:#2e7297; margin-top:9px; font-size:16px;">Viktige lenker</h2>
+    		<a href="#" style="font-size:12px; line-height: 160%;">› Registrer et prosjekt</a><br />
+			<a href="#" style="font-size:12px; line-height: 160%;">› Logg inn</a><br />
+			<a href="#" style="font-size:12px; line-height: 160%;">› Prosjektveiviseren.no</a><br />
+			<a href="#" style="font-size:12px; line-height: 160%;">› Kontakt oss</a><br />
+			<a href="#" style="font-size:12px; line-height: 160%;">› Om Difi</a><br />
+			</div>​
+    	</div> 
+	</div> 
+
+</div>
+
+	</body>
+</html>
