92d20fdb8db406f9d7c209da4e67090a57f57239
pledgeco <vegard1990@gmail.com>
2015-04-02 01:16:11 +0200

    Added seperated menu

---
+++ includes/header.php
@@ -0,0 +1,58 @@
+<style type="text/css">
+
+#menu {
+	float:right;
+
+}
+
+ul#nav li { /*Setter at liste elementet ikke skal ha noen stil på seg. Setter også at det skal være 15 pixler mellom hver lenke i meny*/
+	list-style-type: none;
+	display: inline-block;
+	margin-left: 15px;
+	margin-top:51px;
+}
+
+a:link {
+    text-decoration: none;
+}
+
+a:visited {
+    text-decoration: none;
+}
+
+a {	
+	font-size:13px;
+	color:#111111;
+	
+}
+
+a:hover {
+	color:#868889;
+
+}
+
+#logo {
+	background-image: url("images/logo2.png");
+	height:60px;
+	width:350px;
+	float:left;
+	margin-top:15px;
+
+}
+
+</style>
+
+	<a id="logo" href="http://prosjektdatabasen.com"></a>
+
+<div id="menu">
+	<ul id="nav"> <!--  meny  -->
+                <li><a href="index"> Hjem</a></li>
+                <li><a href="register"> Registrer et prosjekt</a></li>
+                <li><a href="#"> Kontakt oss</a></li>
+		<li><a href="#" data-reveal-id="#"> Logg inn</a></li>
+	</ul>          
+</div>
+
+		<br /><br /><br /><br /><br /><br /><br />
+	<hr />
+<br />
